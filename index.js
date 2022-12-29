c508fc1b1c1b9dd9b0557484bcfad2d11c8f8d99
Andrew Longhorn <Andrew_Longhorn@TrendMicro.com>
2022-12-29 14:50:44 +1000

    enable socket half open to give time for the server to respond

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const net = require('net');
 const uuid = require('uuid').v4;
 
+const terminator = Buffer.from(new Uint8Array([0,0,0,0]));
+
+console.log('test: ', terminator);
+
 const newServer = ({ port, sessionHandler, dataHandler, endHandler }) => {
 
-    const server = net.createServer(async function(socket) {
+    const server = net.createServer({
+        allowHalfOpen: true           // need to be able to respond after the client finishes sending
+    }, function(socket) {
 
+        var firstChunk = true;
+        var isSize = true;
         const socketId = uuid();
         socket.id = socketId;
 
@@ -15,15 +23,38 @@ const newServer = ({ port, sessionHandler, dataHandler, endHandler }) => {
         socket.on('readable', async () => {
             let chunk;
             while (null !== (chunk = socket.read())) {
-                // forward this chunk to the AV scanner
-                await dataHandler(socket, chunk);
+                // handle initial chunks
+                if (firstChunk && (chunk.toString() === 'zINSTREAM\0')) {
+                    firstChunk = false;
+                } else {
+                    // drop size packets
+                    // todo: follow the spec and use the size to determine the chunk boundaries
+                    if (isSize) {
+                        isSize = false; // next is data
+                        console.log('data size = ', chunk);
+                        // handle end of data
+                        if (!Buffer.compare(chunk, terminator)) {
+                            console.log('cool!', chunk);
+                            await endHandler(socket);
+                            socket.end();
+                        }
+                    } else {
+                        isSize = true; // next is another size packet
+                        // forward this chunk, it's real data
+                        console.log('data chunk:', chunk);
+                        await dataHandler(socket, chunk);
+                    }
+                }
                 // todo: handle backpressure - maybe this is already handled?
+                
             }
         });
     
-        socket.on('end', () => {
-            socket.pipe(socket);
-            endHandler(socket);
+        socket.on('end', async () => {
+            console.log('socket ended');
+            //await endHandler(socket);
+            //socket.pipe(socket);
+            //endHandler(socket);
             // return {
             //     name: upload.name,
             //     is_infected: result.isInfected,
