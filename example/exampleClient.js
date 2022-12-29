a5ebc681e432035f6ca9f7142992690ac51c5c0b
Andrew Longhorn <Andrew_Longhorn@TrendMicro.com>
2022-12-29 11:40:14 +1000

    switch example client to use clamdjs library

--- example/exampleClient.js
+++ example/exampleClient.js
@@ -1,23 +1,13 @@
-const { Readable } = require('stream');
-const net = require('net');
-const fs = require('fs');
+const clamd = require('clamdjs');
+const scanner = clamd.createScanner('127.0.0.1', 3310);
 
-var client = new net.Socket();
-client.connect(3310, '127.0.0.1', function() {
-	console.log('Connected');
-    const stream = new Readable();
-    var file = fs.readFileSync('test.txt');
-    stream.push(file);
-    stream.push(null);
-    stream.pipe(client);
-});
-
-client.on('data', function(data) {
-    const result = JSON.parse(data);
-    console.log('Received:', result);
-    client.destroy(); // kill client after server's response
-});
-
-client.on('close', function() {
-	console.log('Connection closed');
-});
+scanner.scanFile('test.txt', 3000, 1024 * 1024)
+    .then((reply) => {
+        console.log('reply: ', reply);
+        // print some thing like
+        // 'stream: OK', if not infected
+        // `stream: ${virus} FOUND`, if infected
+    })
+    .catch((error) => {
+        console.error('error: ', error);
+    });
